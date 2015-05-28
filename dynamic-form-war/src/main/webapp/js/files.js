5bb04295624649c64b6b7ea6a8fd35928fbdec12
rogerio <rogerio.rs@gmail.com>
2015-05-28 19:34:05 -0300

    #11 acertos

--- dynamic-form-war/src/main/webapp/js/files.js
+++ dynamic-form-war/src/main/webapp/js/files.js
@@ -1,11 +1,10 @@
-  var app = angular.module("MyApp", []);
+var app = angular.module("MyApp", []);
 
-        app.controller("PostsCtrl", function($scope, $http) {
-          $http.get('rs/v1/formulariofiles').
-            success(function(data, status, headers, config) {
-              $scope.posts = data;
-            }).
-            error(function(data, status, headers, config) {
-              // log error
-            });
-        });
+app.controller("PostsCtrl", function($scope, $http) {
+	$http.get('rs/v1/formulariofiles').success(
+			function(data, status, headers, config) {
+				$scope.posts = data;
+			}).error(function(data, status, headers, config) {
+		// log error
+	});
+});
