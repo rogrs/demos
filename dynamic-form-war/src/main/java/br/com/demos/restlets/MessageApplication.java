9a0c05f6463341676e0bb6a1bf70620b0e798e19
rogerio <rogerio.rs@gmail.com>
2015-06-18 19:18:11 -0300

    #1 add files crud

--- dynamic-form-war/src/main/java/br/com/demos/restlets/MessageApplication.java
+++ dynamic-form-war/src/main/java/br/com/demos/restlets/MessageApplication.java
@@ -7,15 +7,12 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 import br.com.demos.restlets.impl.AuthenticationRestlet;
-import br.com.demos.restlets.impl.CasosTestesRestlet;
 import br.com.demos.restlets.impl.EventosRestlet;
 import br.com.demos.restlets.impl.FormularioFilesRestlet;
 import br.com.demos.restlets.impl.FormulariosRestlet;
 import br.com.demos.restlets.impl.GeolocationRestlet;
 import br.com.demos.restlets.impl.PerguntasRestlet;
-import br.com.demos.restlets.impl.ProjetosRestlet;
 import br.com.demos.restlets.impl.PropriedadesRestlet;
-import br.com.demos.restlets.impl.TarefasRestlet;
 import br.com.demos.restlets.impl.UsuariosRestlet;
 
 
@@ -28,11 +25,9 @@ public class MessageApplication extends Application {
     private Set<Object> singletons = new HashSet<Object>();
 
     public MessageApplication() {
-            singletons.add(new CasosTestesRestlet());
+       
             singletons.add(new EventosRestlet());
-            singletons.add(new ProjetosRestlet());
             singletons.add(new PropriedadesRestlet());
-            singletons.add(new TarefasRestlet());
             singletons.add(new PerguntasRestlet());
             singletons.add(new FormulariosRestlet());
             singletons.add(new UsuariosRestlet());  
